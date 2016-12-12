5602514060777d728f7e3bd50522b01aeb375f07
jtenner <tenner.joshua@gmail.com>
2016-12-12 14:50:46 -0500

    testing

--- test.jsx
+++ test.jsx
@@ -5,7 +5,7 @@ let ctx = document.createElement('canvas').getContext('2d');
 e2d.render(
   <translate x={100} y={100}>
     <path>
-      <rect width={200} height={50}></rect>
+      <rect width={200} height={50} />
     </path>
     <lineStyle miterLimit={2} >
       <stroke />
