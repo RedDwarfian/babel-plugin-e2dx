16a55243f9e3ba13b09ad65c4bbac5a3a1442157
jtenner <tenner.joshua@gmail.com>
2017-01-10 12:46:11 -0500

    make fillText x and y properties optional

--- dist/babel-plugin-e2dx.js
+++ dist/babel-plugin-e2dx.js
@@ -396,21 +396,25 @@ module.exports = (props, children, loc, types, name) => {
 let warning = __webpack_require__(0);
 
 module.exports = (props, children, loc, types, name) => {
+  let hasPoint = !!(props.x || props.y);
+
+  if (hasPoint)  {
+    if (!props.x) {
+      warning('x', 'e2d', name, loc, '0');
+      props.x = types.numericLiteral(0);
+    }
+
+    if (!props.y) {
+      warning('y', 'e2d', name, loc, '0');
+      props.y = types.numericLiteral(0);
+    }
+  }
 
   if (!props.text) {
     warning('text', 'e2d', name, loc, '""');
     props.text = types.stringLiteral("");
   }
 
-  if (!props.x) {
-    warning('x', 'e2d', name, loc, '0');
-    props.x = types.numericLiteral(0);
-  }
-
-  if (!props.y) {
-    warning('y', 'e2d', name, loc, '0');
-    props.y = types.numericLiteral(0);
-  }
 };
 
 /***/ },
